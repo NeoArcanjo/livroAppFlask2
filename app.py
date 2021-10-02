f0fe89f185836e5ab02e37481170879599f4e9e1
Sylon <rexmundi.rafael@gmail.com>
2021-10-02 12:55:02 -0300

    new file:   templates/cap_list.html

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template, request
-from rotinas import int_to_Roman, loadDados
+from flask import Flask, render_template, request, url_for
+from rotinas import intToRoman, loadDados
 
 app = Flask(__name__)
 
+
 @app.route('/detalhes/')
-def hello_world():  # put application's code here
+def detalhes():  # put application's code here
     cp = request.args.get('cap')
     busca = request.args.get('pesquisaTermo')
     livro = loadDados(cp)
     encontrado = False
-    if busca is not None:
+    print(busca)
+    if busca not in [None, "", '']:
         if busca in livro:
             encontrado = True
     else:
         busca = ''
     return render_template('app.html', texto=livro, cp=cp, busca=busca, encontrado=encontrado)
 
+
 @app.route('/')
 def index():
     dados = loadDados()
     return render_template('index.html', dados=dados)
+
+
+# @app.route('/<path:dummy>')
+# def fallback(dummy=None):
+#     return redirect(url_for('index'))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('not_found.html')
+
 
 if __name__ == '__main__':
     app.run()
--- rotinas.py
+++ rotinas.py
@@ -1,6 +1,7 @@
 import requests
 
-def int_to_Roman(num):
+
+def intToRoman(num):
     val = [
         1000, 900, 500, 400,
         100, 90, 50, 40,
@@ -22,17 +23,21 @@ def int_to_Roman(num):
         i += 1
     return roman_num
 
+
 def loadDados(index=''):
-    dados = requests.get('https://raw.githubusercontent.com/grovina/assisnet/master/casmurro.txt').text
+    dados = requests.get(
+        'https://raw.githubusercontent.com/grovina/assisnet/master/casmurro.txt').text
     conteudo = {}
     for cap in range(1, 149):
-        rom_cap_inicio = int_to_Roman(cap)
-        rom_cap_fim = int_to_Roman(cap + 1)
+        rom_cap_inicio = intToRoman(cap)
+        rom_cap_fim = intToRoman(cap + 1)
         #print(rom_cap_inicio + '>>' + rom_cap_fim)
         if cap == 1:
-            capitulo = dados.split('\n' + rom_cap_inicio)[0].split('\n' + rom_cap_fim)[0]
+            capitulo = dados.split(
+                '\n' + rom_cap_inicio)[0].split('\n' + rom_cap_fim)[0]
         else:
-            capitulo = dados.split('\n' + rom_cap_inicio)[1].split('\n' + rom_cap_fim)[0]
+            capitulo = dados.split(
+                '\n' + rom_cap_inicio)[1].split('\n' + rom_cap_fim)[0]
         conteudo['CAP: ' + rom_cap_inicio + ' >> ' + capitulo.replace('\n', '').replace('\r', '')[0:20].split('.')[0]] = \
             capitulo.replace('\n', '<br>').replace('\r', '')
     if index != '':
